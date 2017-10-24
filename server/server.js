42dd114d24f95361d8c716294ef4114c3ed39f65
danielromanich <danielromanich@users.noreply.github.com>
2017-10-24 23:01:34 +0200

    Use mongoose

--- server/server.js
+++ server/server.js
@@ -6,7 +6,15 @@ var express = require('express'),
     bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://gruppe50:gruppe50@it2810-shard-00-00-qogwh.mongodb.net:27017,it2810-shard-00-01-qogwh.mongodb.net:27017,it2810-shard-00-02-qogwh.mongodb.net:27017/test?ssl=true&replicaSet=IT2810-shard-0&authSource=admin');
+mongoose.connect('mongodb://gruppe50:gruppe50@it2810-shard-00-00-qogwh.mongodb.net:27017,it2810-shard-00-01-qogwh.mongodb.net:27017,it2810-shard-00-02-qogwh.mongodb.net:27017/test?ssl=true&replicaSet=IT2810-shard-0&authSource=admin',
+    {
+        useMongoClient: true
+    });
+const db = mongoose.connection;
+
+db.once("open", () => {
+    console.log("Connected to database");
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
