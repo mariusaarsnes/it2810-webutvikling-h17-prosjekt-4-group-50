0df38a12db9d7d585ae4d9853a3193e4e26a7f3f
danielromanich <danielromanich@users.noreply.github.com>
2017-10-24 23:03:59 +0200

    Change to localhost

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ var express = require('express'),
     Song = require("./models/SongModel"),
     bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://gruppe50:gruppe50@it2810-shard-00-00-qogwh.mongodb.net:27017,it2810-shard-00-01-qogwh.mongodb.net:27017,it2810-shard-00-02-qogwh.mongodb.net:27017/test?ssl=true&replicaSet=IT2810-shard-0&authSource=admin',
+mongoose.Promise = global.Promise; //'mongodb://gruppe50:gruppe50@it2810-shard-00-00-qogwh.mongodb.net:27017,it2810-shard-00-01-qogwh.mongodb.net:27017,it2810-shard-00-02-qogwh.mongodb.net:27017/test?ssl=true&replicaSet=IT2810-shard-0&authSource=admin
+mongoose.connect('mongodb://localhost:27017/test',
     {
         useMongoClient: true
     });
