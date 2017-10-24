bc2562b227a7638b5923843bf75453f8159b70c7
danielromanich <daniel@romanich.com>
2017-10-24 19:24:16 +0200

    Small api start

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test');
+/*var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');*/
+var MongoClient = require('mongodb').MongoClient;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -11,8 +12,31 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    res.json({message: 'hooray! welcome to our api!'});
+router.get('/name/:name/', function (req, res) {
+    MongoClient.connect('mongodb://gruppe50:gruppe50@it2810-shard-00-00-qogwh.mongodb.net:27017,it2810-shard-00-01-qogwh.mongodb.net:27017,it2810-shard-00-02-qogwh.mongodb.net:27017/test?ssl=true&replicaSet=IT2810-shard-0&authSource=admin', function(err, db) {
+        console.log(err);
+        console.log("Connected correctly to database");
+        console.log(db.collection("test").findOne({name: req.params.name}, function(err, item) {
+            res.json(item);
+        }));
+        db.close();
+    });
+});
+
+router.get('/names', function (req, res) {
+    MongoClient.connect('mongodb://gruppe50:gruppe50@it2810-shard-00-00-qogwh.mongodb.net:27017,it2810-shard-00-01-qogwh.mongodb.net:27017,it2810-shard-00-02-qogwh.mongodb.net:27017/test?ssl=true&replicaSet=IT2810-shard-0&authSource=admin', function(err, db) {
+        console.log(db.collection("test").find(function(err, item) {
+            var names = [];
+            item.forEach(function(data) {
+                if (data !== undefined) {
+                    names.push(JSON.stringify(data));
+                }
+            }, function() {
+                res.json(names);
+            });
+        }));
+        db.close();
+    });
 });
 
 app.use('/api', router);
