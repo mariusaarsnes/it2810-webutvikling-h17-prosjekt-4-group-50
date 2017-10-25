dca25341e62865c2f5a93cb2b19e08888f8523c2
mariusaarsnes <marius.aarsnes@gmail.com>
2017-10-25 14:26:07 +0200

    remove port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ let express = require('express'),
     bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise; //
-mongoose.connect('mongodb://localhost:27017/test',
+mongoose.connect('mongodb://localhost/test',
     {
         useMongoClient: true
     }, (err) => {
