11f85439186c03a979a85e4cf16c99e17f681472
danielromanich <danielromanich@users.noreply.github.com>
2017-10-30 20:15:47 +0100

    Add comments to server.js

--- server/server.js
+++ server/server.js
@@ -37,6 +37,9 @@ passport.deserializeUser((user, done) => {
     done(null, user);
 });
 
+/**
+ * Provides a LocalStrategy to passport that checks if a user provided the correct username/password for login.
+ */
 passport.use(new LocalStrategy(
     (username, password, done) => {
         User.findOne({username: username}, (err, user) => {
@@ -57,6 +60,12 @@ app.use(session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+/**
+ * Checks wether the user is logged in or not, will display a message if it failed to authorize user
+ * @param req
+ * @param res
+ * @param next
+ */
 isAuthorized = (req, res, next) => {
     if (req.user)
         next();
@@ -72,6 +81,9 @@ app.get('/create_user', (req, res) => {
     res.sendFile(path.join(__dirname+'/CreateUser.html'));
 });
 
+/**
+ * Used to maintain a user session. Uses the LocalStrategy provided to passport.
+ */
 app.post('/login',
     passport.authenticate('local', {
         successRedirect: '/logged_in/failed/false/message/Successfully logged in!',
@@ -79,17 +91,28 @@ app.post('/login',
     })
 );
 
+/**
+ * Called by the authentication function and returns a json object containing if it managed
+ * to log in or not, aswell as a message with more information
+ */
 app.get('/logged_in/failed/:failed/message/:message', (req, res) => {
     res.json(req.params);
 });
 
+/**
+ * Called by various controllers and returns a json object with a message
+ */
 app.get('/message/:message', (req, res) => {
     res.json(req.params);
 });
 
+/**
+ * All the routers for the different parts of the database. The user router also takes in bcrypt, as it needs to
+ * encrypt passwords when new users are created
+ */
 let songRoute = require('./routes/SongRouter'), userRoute = require('./routes/UserRouter');
 songRoute(app, isAuthorized);
-userRoute(app, isAuthorized, bcrypt);
+userRoute(app, isAuthorized, bcrypt); 
 
 app.listen(port);
 console.log('Server running on port: ' + port);
