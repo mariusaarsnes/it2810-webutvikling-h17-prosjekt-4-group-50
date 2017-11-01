ea0e40ad2f072691f6fdd67478ad8b229b1c879b
Westermoen <jakob@westermoen.com>
2017-11-01 14:37:42 +0100

    add components folder to app

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
