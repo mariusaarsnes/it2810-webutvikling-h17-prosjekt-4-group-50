99505d0d8f178a521e9b77e2b017fd25b96879f1
Freddysouth <fredd-sormo-1995@hotmail.com>
2017-11-01 20:40:02 +0100

    add top functionality to search component

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -5,19 +5,23 @@ import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import {RouterModule} from '@angular/router';
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot([
       {
-        path: 'navbar',
-        component: NavbarComponent
+        path: 'search',
+        component: SearchPageComponent
       }
     ])
   ],
