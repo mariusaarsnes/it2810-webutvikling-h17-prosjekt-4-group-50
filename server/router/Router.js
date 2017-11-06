9f3a753f45cefec0cd3f44ae548c04029ce324d3
danielromanich <daniel@romanich.com>
2017-11-06 21:11:34 +0100

    Add general query request for artist

--- server/router/Router.js
+++ server/router/Router.js
@@ -56,6 +56,8 @@ module.exports = (isAuthorized, isAdmin, passport) => {
     /**
      * Artist related API queries
      */
+    router.get("/artists/:search_string/:sort/:type/:filter/:filter_value/:index/:amount", artistController.findArtistsTest);
+
     router.get("/artists/:index/:amount", artistController.findAllArtists);
     router.get("/artists/:search_string/:index/:amount", artistController.findArtists)
         .put("/artists/:search_string/:index/:amount", userController.updateSearchHistory);
