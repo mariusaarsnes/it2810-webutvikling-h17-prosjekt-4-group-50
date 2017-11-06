ec582526b87a36f3c8d64d94b9370001a1610330
mayhelenrs <mayhelenrs@icloud.com>
2017-11-06 15:03:34 +0100

    Changes to img link

--- website/src/app/components/artist/mock-artists.ts
+++ website/src/app/components/artist/mock-artists.ts
@@ -1,16 +1,16 @@
 import { Artist } from "./artist";
 
 export const ARTISTS: Artist[] = [
-  {id: 1, name: "Rod Stewart1", tracks: 20, popularity: 100, image: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg"},
-  {id: 2, name: "Rod Stewart2", tracks: 20, popularity: 100, image: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg"},
-  {id: 3, name: "Rod Stewart3", tracks: 20, popularity: 100, image: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg"},
-  {id: 4, name: "Rod Stewart4", tracks: 20, popularity: 100, image: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg"},
-  {id: 5, name: "Rod Stewart5", tracks: 20, popularity: 100, image: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg"},
-  {id: 6, name: "Rod Stewart6", tracks: 20, popularity: 100, image: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg"},
-  {id: 7, name: "Rod Stewart7", tracks: 20, popularity: 100, image: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg"},
-  {id: 8, name: "Rod Stewart8", tracks: 20, popularity: 100, image: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg"},
-  {id: 9, name: "Rod Stewart9", tracks: 20, popularity: 100, image: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg"},
-  {id: 10, name: "Rod Stewart10", tracks: 20, popularity: 100, image: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg"},
-  {id: 11, name: "Rod Stewart11", tracks: 20, popularity: 100, image: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg"},
-  {id: 12, name: "Rod Stewart12", tracks: 20, popularity: 100, image: "https://www.biography.com/.image/t_share/MTQ3MzM3MTcxNjA5NTkzNjQ3/ariana_grande_photo_jon_kopaloff_getty_images_465687098.jpg"},
+  {id: 1, name: "Rod Stewart1", tracks: 20, popularity: 100, image: "https://i.scdn.co/image/53988c874e08973d0b12381116541c7153ccf274"},
+  {id: 2, name: "Rod Stewart2", tracks: 20, popularity: 100, image: "https://i.scdn.co/image/53988c874e08973d0b12381116541c7153ccf274"},
+  {id: 3, name: "Rod Stewart3", tracks: 20, popularity: 100, image: "https://i.scdn.co/image/53988c874e08973d0b12381116541c7153ccf274"},
+  {id: 4, name: "Rod Stewart4", tracks: 20, popularity: 100, image: "https://i.scdn.co/image/53988c874e08973d0b12381116541c7153ccf274"},
+  {id: 5, name: "Rod Stewart5", tracks: 20, popularity: 100, image: "https://i.scdn.co/image/53988c874e08973d0b12381116541c7153ccf274"},
+  {id: 6, name: "Rod Stewart6", tracks: 20, popularity: 100, image: "https://i.scdn.co/image/53988c874e08973d0b12381116541c7153ccf274"},
+  {id: 7, name: "Rod Stewart7", tracks: 20, popularity: 100, image: "https://i.scdn.co/image/53988c874e08973d0b12381116541c7153ccf274"},
+  {id: 8, name: "Rod Stewart8", tracks: 20, popularity: 100, image: "https://i.scdn.co/image/53988c874e08973d0b12381116541c7153ccf274"},
+  {id: 9, name: "Rod Stewart9", tracks: 20, popularity: 100, image: "https://i.scdn.co/image/53988c874e08973d0b12381116541c7153ccf274"},
+  {id: 10, name: "Rod Stewart10", tracks: 20, popularity: 100, image: "https://i.scdn.co/image/53988c874e08973d0b12381116541c7153ccf274"},
+  {id: 11, name: "Rod Stewart11", tracks: 20, popularity: 100, image: "https://i.scdn.co/image/53988c874e08973d0b12381116541c7153ccf274"},
+  {id: 12, name: "Rod Stewart12", tracks: 20, popularity: 100, image: "https://i.scdn.co/image/53988c874e08973d0b12381116541c7153ccf274"},
 ];
