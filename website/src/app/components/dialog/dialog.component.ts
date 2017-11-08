bbef963081abbb83f7da9350193c9cd50e8bd843
mayhelenrs <mayhelenrs@icloud.com>
2017-11-08 16:47:43 +0100

    Now has close button

--- website/src/app/components/dialog/dialog.component.ts
+++ website/src/app/components/dialog/dialog.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject} from '@angular/core';
-import {MAT_DIALOG_DATA} from '@angular/material';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 
 @Component({
 	selector: 'app-dialog',
@@ -8,6 +8,9 @@ import {MAT_DIALOG_DATA} from '@angular/material';
 })
 
 export class DialogComponent {
-	constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+	constructor(@Inject(MAT_DIALOG_DATA) public data: any, public thisDialogRef: MatDialogRef<DialogComponent>) {
+	}
+	closeDialog() {
+		this.thisDialogRef.close();
 	}
 }
