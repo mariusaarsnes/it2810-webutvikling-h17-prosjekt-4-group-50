296becd6954edcb4241ac03dae4d3544023410de
mayhelenrs <mayhelenrs@icloud.com>
2017-11-10 11:20:54 +0100

    Merge conflict

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -14,59 +14,56 @@ import {ArtistComponent} from './components/artist/artist.component';
 import {SearchService} from "./components/search-result/search.service";
 import {DialogComponent} from "./components/dialog/dialog.component";
 import {AlbumService} from "./components/artist/album.service";
-import {LoginComponent} from './login/login.component';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {RegisterComponent} from './register/register.component';
 
 import {CanActivateService} from "./shared/auth/can-activate.service";
 import {Permissions} from "./shared/auth/Permissions";
-import {RegisterComponent} from "./register/register.component";
 import {LoginComponent} from "./login/login.component";
 import {NavbarSearchComponent} from './shared/navbar-search/navbar-search.component';
 import {TrackComponent} from "./components/track/track.component";
 
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		ProfileComponent,
-		NavbarComponent,
-		SearchPageComponent,
-		SearchResultComponent,
-		ArtistComponent,
-		DialogComponent,
-		LoginComponent,
-		RegisterComponent,
-		LoginComponent,
-		NavbarSearchComponent,
-		TrackComponent
-	],
-	imports: [
-		BrowserModule,
-		BrowserAnimationsModule,
-		FormsModule,
-		HttpClientModule,
-		MatDialogModule,
-		RouterModule.forRoot([
-			{
-				path: 'search',
-				component: NavbarSearchComponent,
-				//canActivate: [CanActivateService]
+    declarations: [
+        AppComponent,
+        ProfileComponent,
+        NavbarComponent,
+        SearchPageComponent,
+        SearchResultComponent,
+        ArtistComponent,
+        DialogComponent,
+        LoginComponent,
+        RegisterComponent,
+        NavbarSearchComponent,
+        TrackComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientModule,
+        MatDialogModule,
+        RouterModule.forRoot([
+            {
+                path: 'search',
+                component: NavbarSearchComponent,
+                //canActivate: [CanActivateService]
 
-			},
-			{
-				path: 'login',
-				component: LoginComponent,
-			},
-			{
-				path: 'register',
-				component: RegisterComponent,
-			}
-		])
-	],
-	entryComponents: [DialogComponent],
-	providers: [SearchService, AlbumService, CanActivateService, Permissions, HttpClient],
-	bootstrap: [AppComponent]
+            },
+            {
+                path: 'login',
+                component: LoginComponent,
+            },
+            {
+                path: 'register',
+                component: RegisterComponent,
+            }
+        ])
+    ],
+    entryComponents: [DialogComponent],
+    providers: [SearchService, AlbumService, CanActivateService, Permissions, HttpClient],
+    bootstrap: [AppComponent]
 
 })
 
