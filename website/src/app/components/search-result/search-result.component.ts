2bbcdb31dafd1df771cda3868c32799891bb6cb3
danielromanich <daniel@romanich.com>
2017-11-13 15:00:47 +0100

    Fix search

--- website/src/app/components/search-result/search-result.component.ts
+++ website/src/app/components/search-result/search-result.component.ts
@@ -36,7 +36,7 @@ export class SearchResultComponent implements OnInit, OnChanges {
 	}
 
 	public getArtistsByName(): void {
-		this.searchService.getArtists(this.searchString, this.renderTreshold, this.index, this.filterList ? this.filterList.map(array => array[0]).join(",") : "none", this.filterList ? this.filterList.map(array => array[1]).join(",") : "none", this.sort ? this.sort : "none", this.sortType ? this.sortType : "ascending").subscribe(artists => {
+		this.searchService.getArtists(this.searchString, this.renderTreshold, this.index, this.filterList.length > 0 ? this.filterList.map(array => array[0]).join(",") : "none", this.filterList.length > 0 ? this.filterList.map(array => array[1]).join(",") : "none", this.sort ? this.sort : "none", this.sortType ? this.sortType : "ascending").subscribe(artists => {
 			this.artists = artists;
 			this.canRenderNew = true;
 		});
