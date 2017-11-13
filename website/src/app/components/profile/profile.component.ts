bd8114c0b4ec2c068942d76ee10561bb50e7e80e
Westermoen <jakob@westermoen.com>
2017-11-13 12:05:17 +0100

    remove unused mock-data from profile, remove wordcloud from profile

--- website/src/app/components/profile/profile.component.ts
+++ website/src/app/components/profile/profile.component.ts
@@ -11,14 +11,4 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit() {
   }
-  // USER INFO
-  username = "Testbruker123"
-  password = "pass123"
-  since = "06-11-2017"
-
-
-  //SEARCH HISTORY
-  uniqueSearches = 60
-  totalSearches = 104
-  recentSearches = ["Queen", "Johnny Cash", "Rapsody", "Yellow", "Coldplay"]
 }
