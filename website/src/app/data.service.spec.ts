24e61ea08ddeea1c510cc419924a984ef2d5a9a1
Freddysouth <fredd-sormo-1995@hotmail.com>
2017-11-15 15:46:13 +0100

    move data service

--- website/src/app/data.service.spec.ts
+++ website/src/app/data.service.spec.ts
@@ -1,6 +1,6 @@
 import {inject, TestBed} from '@angular/core/testing';
 
-import {DataService} from '../../data.service';
+import {DataService} from './data.service';
 import {HttpClient, HttpHandler} from '@angular/common/http';
 
 describe('DataService', () => {
