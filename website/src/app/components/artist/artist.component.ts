8e5854f2d06429d1e65bcd310ec0faa2a85db29c
mayhelenrs <mayhelenrs@icloud.com>
2017-11-15 10:05:40 +0100

    Songs in dialog

--- website/src/app/components/artist/artist.component.ts
+++ website/src/app/components/artist/artist.component.ts
@@ -2,11 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { DialogComponent } from '../dialog/dialog.component';
 import { ArtistResponse } from "../../interfaces/artist-response.interface";
-import { AlbumResponse } from "../../interfaces/album-response.interface";
 import {SongResponse} from "../../interfaces/song-response.interface";
 import {SongsDialogComponent} from "../songs-dialog/songs-dialog.component";
 import {SearchService} from "../search-result/search.service";
 import {Observable} from "rxjs/Observable";
+import {AlbumResponse} from "../../interfaces/album-response.interface";
 
 @Component({
 	selector: 'app-artist',
@@ -31,35 +31,39 @@ export class ArtistComponent implements OnInit {
         return this.searchService.getAlbumsByIds(albums);
     }
     getSongs(songs): Observable<SongResponse[]> {
-        return this.searchService.getSongsByIdsWithAlbums(songs);
+        return this.searchService.getSongsByIds(songs);
     }
 
 
     openDialogAlbums() {
-        console.log(this.artist);
-        this.getAlbums(this.artist.albums).subscribe(albums => {
-            this.albums = albums;
+        this.getSongs(this.artist.songs).subscribe(songs => {
+            this.songs = songs;
+            this.getAlbums(this.artist.albums).subscribe(albums => {
+                this.albums = albums;
 
-            const dialogRef = this.dialog.open(DialogComponent, {
+                const dialogRef = this.dialog.open(DialogComponent, {
 
-                height: "80%",
-                width: "70%",
-                data: [this.artist, this.albums],
+                    height: "80%",
+                    width: "70%",
+                    data: [this.artist, this.albums, this.songs],
+                });
+                dialogRef.afterClosed();
             });
-            dialogRef.afterClosed();
         });
+
     }
     openDialogTracks() {
 
         this.getSongs(this.artist.songs).subscribe(songs => {
+
             this.songs = songs;
-            console.log(this.songs);
+
+
 
             const dialogRef = this.dialog.open(SongsDialogComponent, {
-
                 height: "80%",
                 width: "70%",
-                data: [this.artist, this.songs],
+                data: [this.artist],
             });
             dialogRef.afterClosed();
         });
